5e795e7d76719406fc8947e5a034e76b1f9a4b26
bomioz <navarrochristopher068@gmail.com>
2025-06-12 17:36:56 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+const TOKEN = process.env.DISCORD_TOKEN;
+
+client.once('ready', () => {
+  console.log(`🤖 Bot conectado como ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.author.bot) return;
+
+  if (message.content === '!ping') {
+    message.channel.send('¡Pong! 🏓');
+  }
+});
+
+client.login(TOKEN);
